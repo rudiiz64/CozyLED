15f041c08f364c9e04c7ce3b823bef3ce96ac5e7
Rodolfo Zaragoza <62043640+rudiiz64@users.noreply.github.com>
2026-02-20 12:18:49 -0800

    Updated Rev 0.1 code
    Update
    - Squashed some bugs regarding button presses
    - Added button press to LED
    - Debounce needs some work, but buttons not correctly shift through R  -> G -> B and vice versa, depending on what button is pressed
    
    Issues
    - RGB works inversely (false turns on, true turns off) need to debug this
    - Debounce has some issues, primary double pressing when pressed quickly under certain timer, as well as holding down will trigger twice (posedge input)

--- CozyLED v0.1/main/CozyLED.c
+++ CozyLED v0.1/main/CozyLED.c
@@ -30,34 +30,10 @@ volatile unsigned long leftLastBtnTime = 0;
 volatile unsigned long rightBtnTime = 0;
 volatile unsigned long rightLastBtnTime = 0;
 
-void chip_data(void){
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    uint32_t flash_size;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
-           CONFIG_IDF_TARGET,
-           chip_info.cores,
-           (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
-           (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
-           (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
-           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
+// State Declaration
+int CURR_STATE = INIT;
 
-    unsigned major_rev = chip_info.revision / 100;
-    unsigned minor_rev = chip_info.revision % 100;
-    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
-    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-        printf("Get flash size failed");
-        return;
-    }
-
-    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
-           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
-}
-
-static void led_init(){
+static void ledc_init(){
     ledc_timer_config_t ledc_timer = {
         .speed_mode      = LEDC_MODE,               // High or low speed
         .duty_resolution = LEDC_DUTY_RES,           // Max Range of cap, [0, 2 ** duty_resolution]
@@ -99,6 +75,7 @@ void left_btn_isr(){
         LBtn.numberPress++;
         LBtn.pressed = true;
         leftLastBtnTime = leftBtnTime;
+        CURR_STATE = L_COLOR;
     }
 }
 
@@ -108,6 +85,7 @@ void right_btn_isr(){
         RBtn.numberPress++;
         RBtn.pressed = true;
         rightLastBtnTime = rightBtnTime;
+        CURR_STATE = R_COLOR;
     }
 }
 
@@ -124,81 +102,44 @@ static void gpio_init(void){
     gpio_install_isr_service(ESP_INTR_FLAG_LEVEL1);
     gpio_isr_handler_add(L_BTN, left_btn_isr, NULL);
     gpio_set_intr_type(L_BTN, GPIO_INTR_POSEDGE);
-
     gpio_isr_handler_add(R_BTN, right_btn_isr, NULL);
     gpio_set_intr_type(R_BTN, GPIO_INTR_POSEDGE);
 
     // Initialization of the peripherals
     gpio_set_direction(LED_PWR, GPIO_MODE_OUTPUT);
-    gpio_set_direction(L_BTN, GPIO_MODE_INPUT);
+    gpio_set_level(LED_R, true);
+    gpio_set_level(LED_G, true);
+    gpio_set_level(LED_B, true);
+    gpio_set_direction(L_BTN, GPIO_MODE_INPUT);    
     gpio_input_enable(L_BTN);
     gpio_set_direction(R_BTN, GPIO_MODE_INPUT);
     gpio_input_enable(R_BTN);
 }
 
-
-
 void app_main(void){
-    /** Interrupts **/
-    gpio_init();
-    printf("Initialization done\n");
+    struct led_setting led_color;
+    // LED FSM
     while(1){
-        printf("Waiting for button press\n");
-        if (LBtn.pressed == true){
-            printf("Left button pressed %lu times\n", LBtn.numberPress);
-            LBtn.pressed = false;
-        }
-        else if (RBtn.pressed == true){
-            printf("Right button pressed %lu times\n", RBtn.numberPress);
-            RBtn.pressed = false;
-        }
-        vTaskDelay(50);
-    }
-    
-    //gpio_set_intr_type(R_BTN, GPIO_INTR_ANYEDGE);
-    
-
-    /**************************************************************/
-    //struct led_setting led_color;
-    // State Declaration
-    //int CURR_STATE = INIT;
-
-    /*while(1){
-        printf("State machine\n");
-        switch (CURR_STATE)
-        {
+        switch (CURR_STATE){
         case INIT:
             printf("Initializing, state: %d\n", CURR_STATE);
             //led_init();
             gpio_init();
             led_color.pin = LED_R;
-            gpio_set_level(led_color.pin, false);
             gpio_set_level(LED_PWR, true);
             CURR_STATE = DEFAULT;
-            vTaskDelay(100);
-            
+            vTaskDelay(10);
             break;
         
         case DEFAULT:
-            printf("Setting color, state: %d\n", CURR_STATE);
-            printf("Pin: %d\n", led_color.pin);
-            //CURR_STATE = DEFAULT;
-            //gpio_set_level(led_color.pin, false);
-            // If left button is pressed, change state
-            //if (L_BTN){
-            //    CURR_STATE = L_BTN;
-            //}
-            // If right button is pressed, change state
-            //else if (R_BTN){
-             //   CURR_STATE = R_BTN;
-            //}
-            vTaskDelay(100);
+            gpio_set_level(led_color.pin, false);
+            vTaskDelay(10);
             break;
 
         case L_COLOR:
             printf("Left button pressed, state: %d\n", CURR_STATE);
             // If L button has been pressed, change color to previous color
-            gpio_set_level(led_color.pin, false);
+            gpio_set_level(led_color.pin, true);
             if (led_color.pin == LED_R){
                 led_color.pin = LED_B;
             }
@@ -210,13 +151,13 @@ void app_main(void){
             }
             // After actions, return to default state
             CURR_STATE = DEFAULT;
-            vTaskDelay(100);
+            vTaskDelay(10);
             break;
 
         case R_COLOR:
             printf("Right button pressed, state: %d\n", CURR_STATE);
             // If right button is pressed, change color to next color
-            gpio_set_level(led_color.pin, false);
+            gpio_set_level(led_color.pin, true);
             if (led_color.pin == LED_R){
                 led_color.pin = LED_G;
             }
@@ -228,7 +169,7 @@ void app_main(void){
             }
             // After actions, return to default state
             CURR_STATE = DEFAULT;
-            vTaskDelay(100);
+            vTaskDelay(10);
             break;
 
         default:
@@ -237,7 +178,7 @@ void app_main(void){
             break;
         }
         
-        // Breathing Function
+        /*// Breathing Function
         int LED_Duty = LEDC_MAX_RES;
         int dutyChange = (LEDC_MAX_RES / (2 * LEDC_DUTY_RES));
         for (;;){
@@ -257,6 +198,6 @@ void app_main(void){
 
             vTaskDelay(100 / portTICK_PERIOD_MS);
             LED_Duty -= dutyChange;
-        }
-    }*/
+        }*/
+    }
 }
